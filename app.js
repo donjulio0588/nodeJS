bad8a81da325ec8f2799a86b312ccafe501fbd82
DonJulio <donjulio0588@gmail.com>
2021-09-05 11:41:00 -0400

    1er

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+//motor de plantillas
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Titulo dinamico'})
+})
+
+app.get('/servicios', (req, res) => {
+    res.render('services', {serviceTitle: 'Titulo de servicio'})
+})
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {
+        title: 'Error, documento no encontrado',
+        desc: 'No se ha podido encontrar su pagina'
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log('Servidor escuchando por el puerto 3000 !!')
+})
+
---
+++ frutas.js
@@ -0,0 +1,7 @@
+const frutas = ['Platano', 'Manzana', 'Pera', 'Mango', 'Melon']
+const dinero = 1000
+
+module.exports = {
+    frutas,
+    dinero
+}
